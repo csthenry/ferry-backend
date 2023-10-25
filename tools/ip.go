bd1163e58352db662abf315b786c232faeeabf8a
henry <csthenry@foxmail.com>
2023-10-25 10:29:40 +0800

    fix: 修复失效的接口

--- tools/ip.go
+++ tools/ip.go
@@ -1,47 +1,55 @@
 package tools
 
 import (
+	"bytes"
 	"encoding/json"
 	"ferry/pkg/logger"
 	"io/ioutil"
 	"net/http"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 	"github.com/spf13/viper"
 )
 
+func GbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, err := ioutil.ReadAll(reader)
+	return d, err
+}
+
 func GetLocation(ip string) string {
 	var address = "--"
+	type IpAddress struct {
+		Provence  string    `json:"pro"`
+		City      string    `json:"city"`
+	}
 	if viper.GetBool("settings.public.isLocation") {
 		if ip == "127.0.0.1" || ip == "localhost" {
 			return "本机地址"
 		}
-		resp, err := http.Get("https://api.vvhan.com/api/getIpInfo?ip=" + ip)
+		resp, err := http.Get("https://whois.pconline.com.cn/ipJson.jsp?ip=" + ip + "&json=true")
 		if err != nil {
 			panic(err)
 		}
 		defer resp.Body.Close()
-		s, err := ioutil.ReadAll(resp.Body)
+		raw, err := ioutil.ReadAll(resp.Body)
+		s, err := GbkToUtf8(raw)
 
-		var jsonObj map[string]interface{}
-		var mInfo map[string]string	//地址信息
-		err = json.Unmarshal(s, &jsonObj)
+		var mInfo map[string]string
+		json.Unmarshal(s, &mInfo)
 
-		if err != nil {
-			logger.Error("Get AddressAPI Failed:", err)
-		}
-
-		addressInfo, err := json.Marshal(jsonObj["info"])
-		if jsonObj["success"].(bool) && err == nil {
-			json.Unmarshal([]byte(string(addressInfo)), &mInfo)
-		}
+		var ipAddress IpAddress
+		ipAddress.Provence = mInfo["pro"]
+		ipAddress.City = mInfo["city"]
 
 		if err != nil {
-			logger.Error("AddressJson Marshal Failed:", err)
+			logger.Error("Get IpAddress Failed:", err)
 		}
 
-		if mInfo["prov"] == "" {
-			return "Unknown"
+		if ipAddress.Provence == "" {
+			return "未知"
 		}
-		address = mInfo["prov"] + "-" + mInfo["city"]
+		address = ipAddress.Provence + "-" + ipAddress.City
 	}
 	return address
 }
